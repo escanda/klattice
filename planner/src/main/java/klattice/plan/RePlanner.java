c775f3ca3ee91c1d6991d8d4046b1b84cb5d7eb7
Guillermo Cifuentes <gcifuentes@escandasys.eu>
2023-09-29 07:31:38 +0200

    feat: brought within a lot of rules from Calcite

--- planner/src/main/java/klattice/plan/RePlanner.java
+++ planner/src/main/java/klattice/plan/RePlanner.java
@@ -1,51 +1,21 @@
 package klattice.plan;
 
-import com.google.common.collect.ImmutableList;
 import klattice.calcite.DuckDbDialect;
 import klattice.calcite.SchemaHolder;
 import klattice.plan.rule.MagicValuesReplaceRule;
-import org.apache.calcite.config.CalciteSystemProperty;
-import org.apache.calcite.plan.RelOptRule;
+import klattice.plan.rule.Rules;
 import org.apache.calcite.plan.hep.HepPlanner;
 import org.apache.calcite.plan.hep.HepProgram;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.RelRoot;
 import org.apache.calcite.rel.rel2sql.RelToSqlConverter;
 import org.apache.calcite.rel.rel2sql.SqlImplementor;
-import org.apache.calcite.rel.rules.CoreRules;
-import org.apache.calcite.rel.rules.JoinPushThroughJoinRule;
 
 import java.util.List;
 
 import static klattice.substrait.Shared.createSqlToRelConverter;
 
 public class RePlanner {
-    public static final ImmutableList<RelOptRule> BASE_RULES = ImmutableList.of(CoreRules.AGGREGATE_STAR_TABLE,
-            CoreRules.AGGREGATE_PROJECT_STAR_TABLE,
-            CalciteSystemProperty.COMMUTE.value()
-                    ? CoreRules.JOIN_ASSOCIATE
-                    : CoreRules.PROJECT_MERGE,
-            CoreRules.FILTER_SCAN,
-            CoreRules.PROJECT_FILTER_TRANSPOSE,
-            CoreRules.FILTER_PROJECT_TRANSPOSE,
-            CoreRules.FILTER_INTO_JOIN,
-            CoreRules.JOIN_PUSH_EXPRESSIONS,
-            CoreRules.AGGREGATE_EXPAND_DISTINCT_AGGREGATES,
-            CoreRules.AGGREGATE_EXPAND_WITHIN_DISTINCT,
-            CoreRules.AGGREGATE_CASE_TO_FILTER,
-            CoreRules.AGGREGATE_REDUCE_FUNCTIONS,
-            CoreRules.FILTER_AGGREGATE_TRANSPOSE,
-            CoreRules.PROJECT_WINDOW_TRANSPOSE,
-            CoreRules.MATCH,
-            CoreRules.JOIN_COMMUTE,
-            JoinPushThroughJoinRule.RIGHT,
-            JoinPushThroughJoinRule.LEFT,
-            CoreRules.SORT_PROJECT_TRANSPOSE,
-            CoreRules.SORT_JOIN_TRANSPOSE,
-            CoreRules.SORT_REMOVE_CONSTANT_KEYS,
-            CoreRules.SORT_UNION_TRANSPOSE,
-            CoreRules.EXCHANGE_REMOVE_CONSTANT_KEYS,
-            CoreRules.SORT_EXCHANGE_REMOVE_CONSTANT_KEYS);
     private final SchemaHolder schemaHolder;
 
     public RePlanner(SchemaHolder schemaHolder) {
@@ -64,7 +34,12 @@ public class RePlanner {
     public List<RelNode> optimizeRelNodes(List<RelNode> relRoots) {
         var hepPlanner = new HepPlanner(HepProgram.builder()
                 .addRuleInstance(new MagicValuesReplaceRule(MagicValuesReplaceRule.Config.DEFAULT, schemaHolder))
-                .addRuleCollection(BASE_RULES)
+                .addRuleCollection(Rules.CALC_RULES)
+                .addRuleCollection(Rules.BASE_RULES)
+                .addRuleCollection(Rules.ABSTRACT_RULES)
+                .addRuleCollection(Rules.ABSTRACT_RELATIONAL_RULES)
+                .addRuleCollection(Rules.CONSTANT_REDUCTION_RULES)
+                .addRuleCollection(Rules.MATERIALIZATION_RULES)
                 .build());
         return relRoots.stream().map(relNode -> {
             hepPlanner.setRoot(relNode);
